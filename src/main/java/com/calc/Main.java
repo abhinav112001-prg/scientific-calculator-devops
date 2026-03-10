cac9903ce227404a6099c80739889f74de2cd4d2
abhinav112001-prg <abhinav112001@gmail.com>
2026-03-10 16:39:29 +0530

    test commit 2

--- src/main/java/com/calc/Main.java
+++ src/main/java/com/calc/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // this is a test comment
+
         while (true) {
             System.out.println("\nSCIENTIFIC CALCULATOR");
             System.out.println("1. Square Root");
